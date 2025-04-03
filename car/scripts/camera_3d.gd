2368500a176ddb0b8e7ce77b88b7139ab7dc3bb4
tintu <pranav.ajay2000@gmail.com>
2025-04-03 22:25:09 +0530

    Halfway there i g 😅

---
+++ car/scripts/camera_3d.gd
@@ -0,0 +1,14 @@
+extends Camera3D
+
+@export var lerp_speed = 1
+@export var offset = Vector3.ZERO
+@export var target : Node
+
+
+func _physics_process(delta):
+	#if !target:
+		#return
+	#var target_pos = target.global_transform.translated_local(offset)
+	#global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
+	#look_at(target.global_position, Vector3.UP)
+	pass
