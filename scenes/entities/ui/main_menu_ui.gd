5f5a135264b8907f774f80882716844977ddc407
tintu <pranav.ajay2000@gmail.com>
2026-02-26 22:27:22 +0530

    minor changes in ui main menu

--- scenes/entities/ui/main_menu_ui.gd
+++ scenes/entities/ui/main_menu_ui.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
+func _on_texture_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/tintu_map.tscn")
